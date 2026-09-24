f32675b80ed68e5f70ab83e2bad4ca9c30c3c163
a115222010-shu <a115222010@mail.shu.edu.tw>
2026-09-24 18:00:35 +0800

    Week2

--- 115_09_17/Week1_Test1/obj/Debug/net8.0-windows/Week1_Test1.AssemblyInfo.cs
+++ 115_09_17/Week1_Test1/obj/Debug/net8.0-windows/Week1_Test1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Week1_Test1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69faa73ab1a89ce3568a894c0aef03be88bedc77")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62276d3e67b5e33b3c2fc42393cb44fe179777db")]
 [assembly: System.Reflection.AssemblyProductAttribute("Week1_Test1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Week1_Test1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- 115_09_24/Turorial_2_2/Turorial_2_2/Form1.cs
+++ 115_09_24/Turorial_2_2/Turorial_2_2/Form1.cs
@@ -16,5 +16,15 @@ namespace Turorial_2_2
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hallo");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
